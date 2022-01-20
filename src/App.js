ba0386b2724f3721adbc0b4216becc756ac3e8a7
Lautaro-Gonzalez <lgonzalez@aracargroup.com>
2022-01-20 20:39:16 -0300

    backgroundImage ok

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo2 from "./pp.jfif";
 import Title from "./components/Title";
 
 function App() {
-  console.log(logo2);
+  console.log(logo2);  
+
   return (
-    <div style={{ backgroudImage: logo2 }}>
-      <Title />
+    <div style={{ backgroundImage: `url(${logo2})` }}>
+      <Title />       
     </div>
   );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Title() {
+
   return (
     <header>
-      <h1>2090</h1>
+      <h1 style={{ textAlign: "center"}}>2090</h1>
+      <input placeholder="busca tu pj favorito"></input>    
     </header>
   );
 }
