6d391b63dc559d64bd23e4faa87c1f48182953c5
ariellopezph <ariellopezph@gmail.com>
2022-02-07 12:05:39 -0300

    Probando selecciones pj

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { useState } from "react/cjs/react.development";
-import { useGlobalContext } from '../context'
+import { useGlobalContext } from '../context';
+
 
 function Title() {
   const { allCharacters, selectedCharacter, setSelectedCharacter } = useGlobalContext()
-
+  const [estadoPj, cambiarEstadoPj ] = useState(false);
+  const cambiarPj = () => {
+    cambiarEstadoPj(!estadoPj);
+  }
   return (
-    <header>
+    <div className="header">
       <h1 style={{ textAlign: "center"}}>2090</h1>
-
+      <button onClick={cambiarPj}>Cambiar Pj</button>
       <select
         onChange={ (e) => {
           const pjElegido =  allCharacters[e.target.value]
@@ -22,9 +26,14 @@ function Title() {
         )}
       </select>
       <div>
-        <h1>{selectedCharacter.fullName}</h1>
+
+        <h3>El personaje es:</h3><h3>{selectedCharacter.fullName}</h3>
+        <h3>Pj seleccionado: {estadoPj ? "pj seleccionado" : "sin seleccion de pj"}</h3>
+        <h1>{estadoPj ? "Imagen uno" : "Imagen dos"}</h1>
       </div>
-    </header>
+
+
+    </div>
     
   );
 }
